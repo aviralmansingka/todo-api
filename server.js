c025e5fe6c29cedff0edf85dbbf3ffacf99b4d64
Aviral Mansingka <aviralmansingka@gmail.com>
2016-04-09 15:13:37 -0400

    Refactor API with Underscore

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 
 var todos = [];
-var todoNextId = 1;
+var todoNextId = 1
+var _ = require('underscore');
 
 app.use(bodyParser.json());
 
@@ -12,36 +13,41 @@ app.get('/', function(req,res) {
 	res.send('Todo API root');
 });
 
-app.post('/todos', function(req, res) {
-	var body = req.body;
-	body.id = todoNextId++;
-
-	todos.push(body);
-	console.log("All is well");
-	res.json(body);
-});
-
+// GET /todos
 app.get('/todos', function(req, res) {
 	res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-	todos.forEach(function(todo){
-		if(todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id : todoId});
+
 	if(matchedTodo) {
 		res.json(matchedTodo);
 	}else{
 		res.status(404).send();
 	}
-	// res.send('Asking for todo with id of ' + req.params.id);
 });
 
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = _.pick(req.body, 'completed', 'description');
+	
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description().trim();
 
+	body.id = todoNextId++;
+
+	todos.push(body);
+	console.log("All is well");
+	
+	res.json(body);
+
+});
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);

6953de4e5b74baec75d9c567de55fadadb0549fc
Aviral Mansingka <aviralmansingka@gmail.com>
2016-04-09 15:25:29 -0400

    Add DELETE todos route

--- server.js
+++ server.js
@@ -49,6 +49,19 @@ app.post('/todos', function(req, res) {
 
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id : todoId});
+
+	if (!matchedTodo) {
+		res.send(404).json({"error" : "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
 })

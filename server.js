6f7dcccca0b3a7ae06b9e93e876f0b1fe5b86714
Aviral Mansingka <aviralmansingka@gmail.com>
2016-04-09 14:41:35 -0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description : 'feed the cat',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res) {
 	res.send('Todo API root');
@@ -26,21 +18,28 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	console.log('ID to be found: '+todoId);
 	var matchedTodo;
 	todos.forEach(function(todo){
 		if(todoId === todo.id) {
 			matchedTodo = todo;
 		}
 	});
-	console.log('My matched Todo: ' + JSON.stringify(matchedTodo));
 	if(matchedTodo) {
 		res.json(matchedTodo);
 	}else{
 		res.status(404).send();
 	}
 	// res.send('Asking for todo with id of ' + req.params.id);
-})
+});
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+});
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
